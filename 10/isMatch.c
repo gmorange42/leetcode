f3e19f7dbeab462c7dd33c5f654d2066435aa35c
Guillaume MORANGE <gmorange@e3r1p3.clusters.42paris.fr>
2023-04-18 11:45:25 +0200

    10.c WIP

--- 10/isMatch.c
+++ 10/isMatch.c
@@ -1,31 +1,52 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void	print(char* location, char* s, char* p)
+{
+	printf("%s S %s\n", location, s);
+	printf("%s P %s\n", location, p);
+}
+
 bool	isMatch(char* s, char* p)
 {
+	char	tmp;
+	int	point;
 	while (*p && *s)
 	{
+		print("Before", s, p);
 		if (*p == '*')
 		{
+			point = 0;
+			print("*", s, p);
 			while (*p == '*')
+				++p;
+			while (*p == '.')
 			{
 				++p;
-				if (*p == '.')
-					++p;
+				++point;
 			}
-			while (*s != *p && *s)
+			tmp = *s;
+			while (*s != *p && *s == tmp)
 				++s;
+			s -= point;
+			p -= point;
+			print("*", s, p);
+//			while (*s != *p && *s)
+//				++s;
 		}
 		else if (*p == '.')
 		{
+			print(".", s, p);
 			++s;
 			++p;
+			print(".", s, p);
 		}
 		else
 		{
 			++s;
 			++p;
 		}
+		puts("");
 	}
 	if (*s == *p)
 		return (true);
